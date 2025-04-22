ea2c24aa3a8243c5c87d0f101411359036840829
WendelRafael <wendelrbarbosa32@gmail.com>
2025-04-22 20:00:02 -0300

    identação code

--- optm.py
+++ optm.py
@@ -6,7 +6,6 @@ print("Iniciando a Otimização")
 
 #Limpando os dados temporários
 temp_dir = os.getenv('TEMP')
-
 if os.path.exists(temp_dir):
     shutil.rmtree(temp_dir, ignore_errors=True)
     print("Pasta Temp limpa com sucesso!")
@@ -15,19 +14,13 @@ else:
 
 #Reiniciando drivers gráficos
 def reiniciar_drivers_graficos():
-    
     for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'] == 'dwm.exe':
-           
             os.system("powershell -Command \"Start-Process -FilePath powershell -ArgumentList 'Stop-Process -Name dwm -Force' -Verb RunAs\"")
-           
             os.system("timeout /t 1")
-    
             os.system("powershell -Command \"Start-Process -FilePath C:\\Windows\\System32\\dwm.exe\"")
             break
-
 reiniciar_drivers_graficos()
 print("Drivers reiniciados")
-
 
 print("Otimização concluida!")
