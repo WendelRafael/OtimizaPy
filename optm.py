5d3e936831c520f1ceec1139cfe98a2c59239f3c
WendelRafael <wendelrbarbosa32@gmail.com>
2025-04-21 21:24:45 -0300

    Drivers Reiniciando

--- optm.py
+++ optm.py
@@ -1,14 +1,33 @@
 import os
 import shutil
+import psutil
 
-# Defina o caminho da pasta Temp
+print("Iniciando a Otimização")
+
+#Limpando os dados temporários
 temp_dir = os.getenv('TEMP')
 
-# Verifique se a pasta Temp existe
 if os.path.exists(temp_dir):
-    # Apague todo o conteúdo da pasta Temp
     shutil.rmtree(temp_dir, ignore_errors=True)
-
     print("Pasta Temp limpa com sucesso!")
 else:
     print("Pasta Temp não encontrada.")
+
+#Reiniciando drivers gráficos
+def reiniciar_drivers_graficos():
+    
+    for proc in psutil.process_iter(['pid', 'name']):
+        if proc.info['name'] == 'dwm.exe':
+           
+            os.system("powershell -Command \"Start-Process -FilePath powershell -ArgumentList 'Stop-Process -Name dwm -Force' -Verb RunAs\"")
+           
+            os.system("timeout /t 1")
+    
+            os.system("powershell -Command \"Start-Process -FilePath C:\\Windows\\System32\\dwm.exe\"")
+            break
+
+reiniciar_drivers_graficos()
+print("Drivers reiniciados")
+
+
+print("Otimização concluida!")
